5c2e3a98e97cb1ca57cefadd418771b3e2d1018a
Marcella <marcellaacoelho@gmail.com>
2025-03-14 23:42:12 -0300

    Cliente e Produto

--- frontend/frontend-gerenciador/src/App.jsx
+++ frontend/frontend-gerenciador/src/App.jsx
@@ -4,6 +4,10 @@ import Inicial from './pages/Inicial'
 import FornecedorForm from './pages/Fornecedor/FornecedorForm'
 import FornecedorList from './pages/Fornecedor/FornecedorList'
 import Navbar from './components/Navbar'
+import ProdutoForm from './pages/Produto/ProdutoForm'
+import ProdutoList from './pages/Produto/ProdutoList'
+import ClienteForm from './pages/Cliente/ClienteForm'
+import ClienteList from './pages/Cliente/ClienteList'
 
 const App = () => {
   return (
@@ -14,6 +18,12 @@ const App = () => {
         <Route path="/listar-fornecedores" element={<FornecedorList />} />
         <Route path="/add-fornecedores" element={<FornecedorForm />} />
         <Route path="/edit-fornecedores/:id" element={<FornecedorForm />} />
+        <Route path="/add-produtos" element={<ProdutoForm />} />
+        <Route path="/listar-produtos" element={<ProdutoList />} />
+        <Route path="/edit-produtos/:id" element={<ProdutoForm />} />
+        <Route path="/add-clientes" element={<ClienteForm />} />
+        <Route path="/edit-clientes/:id" element={<ClienteForm />} />
+        <Route path="/listar-clientes" element={<ClienteList />} />
       </Routes>
     </BrowserRouter>
   )
